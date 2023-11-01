9aebe9d4d8315a91dd6ad3df7a294c32ff41eac2
Michael Steinle <michael.steinle@gmail.com>
2023-11-01 09:12:42 +0100

    get rid of delay, use microseconds to check time

--- 7seg_clock/7seg_clock.ino
+++ 7seg_clock/7seg_clock.ino
@@ -1,18 +1,13 @@
+#include <EEPROM.h>
 #include <Wire.h>
 
-#define DCF_PIN 2
+#define DCF_PIN PD2
 
-//dcf state
-int start = 0;
-int end = 0;
-int duration = 0;
- 
-bool signal = false;
-bool sync = false;
-int count = -1;
-int data[65];
 int hour = 0;
 int minute = 0;
+
+unsigned long last_time = 0;
+
 
 //LED driver adress
 const uint8_t addr = 0x70;
@@ -22,13 +17,8 @@ const int ds3231 = 0x68;
 uint16_t displayBuffer[8];
 
 void setup() {
-
   //start serial for debug
   Serial.begin(2000000);
-  while (!Serial) {;}
-
-  //init dcf
-  pinMode(DCF_PIN, INPUT);
 
   //init matrix-led driver
   //Make connection to LED-driver
@@ -57,60 +47,64 @@ void setup() {
 }
 
 void loop() {
-  if(count > 60){ //somehow we got more than 60 bits
-    sync = false;
-  }
-  int DCF_SIGNAL = digitalRead(DCF_PIN);
-  
-  if (DCF_SIGNAL == HIGH && signal == false){ //rising edge
-    signal = true;
-    end = millis();
-    duration = end - start;
+  unsigned long period = getMspm();
+  unsigned long micros = getMspm();
 
-    if (sync == true) {
-      data[count] = (bitFromTime(duration));
-      Serial.print (data[count]);
+  if(micros - last_time > period){
+    last_time = micros;
+
+    minute += 1;
+    if(minute == 60){
+      minute = 0;
+      hour += 1;
+      if(hour == 24){
+        hour = 0;
+      }
     }
-    else {
-      Serial.print(".");
+
+    Serial.println((hour<10?"0":"") + String(hour) + ":" + (minute<10?"0":"") + String(minute) + " - " + String(getMspm()));
+
+    displayBuffer[0] = to7seg(minute % 10);
+    displayBuffer[1] = to7seg(minute / 10);
+    displayBuffer[2] = to7seg(hour % 10);
+    displayBuffer[3] = to7seg(hour / 10);
+    setBright(calcBright(hour));
+    show();
+  }
+
+  while (Serial.available() > 0) {
+    String line = Serial.readStringUntil('\n');
+    if (line.startsWith("t")) {
+      if (line.length() == 6 && line.charAt(3) == ':') {
+        int hz = line.charAt(1) - 48;
+        int he = line.charAt(2) - 48;
+        int mz = line.charAt(4) - 48;
+        int me = line.charAt(5) - 48;
+        if (hz * 10 + he < 24 && mz < 6 && me < 10 && hz * 10 + he >= 0 && mz >= 0 && me >= 0) {
+          hour = hz * 10 + he;
+          minute = mz * 10 + me;
+          setBright(calcBright(hour));
+           Serial.println((hour<10?"0":"") + String(hour) + ":" + (minute<10?"0":"") + String(minute));
+
+        } else {
+          Serial.println("correct: tXX:XX, got: " + line);
+        }
+      } else {
+        Serial.println("correct: tXX:XX, got: " + line);
+      }
+    }
+    if (line.startsWith("f")) {
+      long msPm2 = line.substring(1).toInt();
+      if(msPm2 > 0){
+        setMspm(msPm2);
+        unsigned long t = getMspm();
+        Serial.println("msPm is now: " + String(t));
+      }else{
+        Serial.println("msPm value out of bounds");
+      }
     }
   }
 
-  if (DCF_SIGNAL == LOW && signal == true) { //falling edge
-    signal = false;
-    start = millis();
-
-    if (duration >= 1700) {
-      count = 0;
-      sync = true;
-      hour = data[29]*1+data[30]*2+data[31]*4+data[32]*8+data[33]*10+data[34]*20;
-      minute = data[21]*1+data[22]*2+data[23]*4+data[24]*8+data[25]*10+data[26]*20+data[27]*40;
-      Serial.println("\n" + String(hour) + ":" + String(minute));
-    } else {
-      count++;
-    }
-  }
-  
-  //ouput time
-  //serial
-  Serial.print(hour, DEC);
-  // convert the byte variable to a decimal number when displayed
-  Serial.print(":");
-  if (minute<10)
-  {
-    Serial.print("0");
-  }
-  Serial.print(minute, DEC);
-  //7seg
-  displayBuffer[0] = to7seg(minute % 10);
-  displayBuffer[1] = to7seg(minute / 10);
-  displayBuffer[2] = to7seg(hour % 10);
-  displayBuffer[3] = to7seg(hour / 10);
-  setBright(calcBright(hour));
-  show();
-
-  //wait
-  delay(500);
 }
 
 //---------------- LED DRIVER STUFF --------------
@@ -156,15 +150,15 @@ void show() {
   Wire.endTransmission();
 }
 
-//-------------------DFC77 stuff ----------------
-int bitFromTime (int duration) {
-  if(duration >= 851 && duration <= 950){
-    return 0;
+unsigned long getMspm(){
+  unsigned long t;
+  EEPROM.get(100, t);
+  if(t > 1000000000){
+    return 1000;
   }
-  if(duration >= 750 && duration <= 850){
-    return 1;
-  }
-  //else, we have a bad connection
-  sync = false;
-  return 0;
+  return t;
+}
+
+void setMspm(unsigned long mspm){
+  EEPROM.put(100, mspm);
 }
